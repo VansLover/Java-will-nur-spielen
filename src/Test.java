9bc819ad5d580f5277c819ccf067ae85c4096d81
VansLover <tobias.kopf@schiller-og.de>
2022-09-22 15:33:16 +0200

    Originalv.1 (Informatik 22.09)

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,12 @@ public class Test {
     public static void main(String[] args) {
         Vertex v1 = new Vertex(1.0, 2.0);
         Vertex v2 = new Vertex(42.0, 37.0);
-        Vertex v3 = new Vertex(0, 0);
-        System.out.println(v1 + " " + v2 + " " + v3);
+        Vertex v3 = v1.skalarMult(); //s:0
         System.out.println(v1.length());
+
+        System.out.println(v1);
+        v1.skalarMultMod();//s:4
+
     }
 }
 
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,11 +1,11 @@
 public class Vertex {
 
     public double x;
-    public double y;
+    public double y; //um von außen daraufzugrifen zu können
 
 
     public Vertex(double x, double y) {
-        this.x = x;
+        this.x = x; //Operatoren für die Vertex-Klasse (Konstruktor)
         this.y = y;
     }
 
@@ -15,8 +15,18 @@ public class Vertex {
     }
 
 
-    @Override
-    public String toString() {
+    @Override //Methode gilt als Überschrieben
+    public String toString() { //Überschreiben der To-String Methode (mit Return-Wert für x und y)
         return "(" + x + ", " + y + ")";
+
+    public Vertex skalarMult(double s) { //Methodename wird klein geschrieben, wenn mehr wie n Wort, dann weitere Wörter mit Großbuchstabe
+        return new Vertex(x*s,y*s);} //double wird genutzt, da man x und y multiplizieren muss (x und y sind schon double) (int kann auch zu einem double conventiert werden)
+
+
+    public void skalarMultMod(double s) {//void damit es kein Rückgabewert gibt
+        this.x=x*s;
+        this.y=y*s;
+    }
+
     }
 }
