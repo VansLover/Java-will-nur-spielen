abf9d1a3c8f04075712962012e9f5ef17655e6db
VansLover <tobias.kopf@schiller-og.de>
2022-09-27 09:12:02 +0200

    Informatik 27.09 last

--- src/Test.java
+++ src/Test.java
@@ -2,12 +2,31 @@ public class Test {
     public static void main(String[] args) {
         Vertex v1 = new Vertex(1.0, 2.0);
         Vertex v2 = new Vertex(42.0, 37.0);
-        Vertex v3 = v1.skalarMult(); //s:0
+        Vertex v3 = new Vertex (34,2);
+        Vertex v4 = new Vertex(42,23)
         System.out.println(v1.length());
 
-        System.out.println(v1);
-        v1.skalarMultMod();//s:4
+        System.out.println(v2.toString());
 
+        System.out.println(v1);
+        v1.skalar(2);
+        System.out.println(v1.length());
+        Vertex v3 = v1.add(v2);
+        System.out.println(v3.length());
+
+        System.out.println(v1.skalarMult(0.8));
+        System.out.println(v1);
+
+        v1.skalarMultMod(0.8);
+        System.out.println(v1)
+
+
+
+        Vertex v1 = new Vertex(1.0, 2.0);
+        System.out.println(v1.equals(v2));
+        System.out.println(v1.equals(v3));
+        System.out.println(v1.equals(v4));
+        System.out.println(v1.equals(v2));
     }
 }
 
