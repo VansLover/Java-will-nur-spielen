39ace7166159bd32e730235e845bc856396ac2ff
VansLover <tobias.kopf@schiller-og.de>
2022-10-04 18:03:14 +0200

    Informatik V.1 Überarbeitung 04.10

--- src/TestTriangle.java
+++ src/TestTriangle.java
@@ -1,8 +1,8 @@
 public class TestTriangle {
-    public static void main (String arg[]){
+    public static void main (String[] args) {
         Vertex v1 = new Vertex(34, 2);
 
-        SimpleTriangle dreieck = new SimpleTriagle(v1, width:100);
+        SimpleTriangle dreieck = new SimpleTriangle(100, v1);
         System.out.println(dreieck);
         System.out.println(dreieck.area());
 
