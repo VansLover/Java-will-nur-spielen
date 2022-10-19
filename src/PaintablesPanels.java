3afa5209aeb7725aef399f447135a629626402d0
VansLover <tobias.kopf@schiller-og.de>
2022-10-19 22:34:22 +0200

    Informatik 19.10 PaintableRectangle, PaintablesPanels und ShowInFrame Veränderungen

--- src/PaintablesPanels.java
+++ src/PaintablesPanels.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class PaintablesPanels extends SizedPanel {
     List<Paintable> pas;//Spitze-Klammern als genauere Übergabe der Methoden
 public PaintablesPanels(List<Paintable> pas) {
-    this.pas=pas;} //Übergabe der List in den Konstruktor
-
+    this.pas = pas; //Übergabe der List in den Konstruktor
+}
 public void paintComponent(java.awt.Graphics g){
-    for(Paintable pa:pas)
-        pa.PaintTo(g); //Listenübergabe auf die Pa-Methode paintTo
+    for(Paintable pa : pas)
+        pa.paintTo(g); //Listenübergabe auf die Pa-Methode paintTo
 }
 
 public static void main(String[] args) {
@@ -18,6 +18,6 @@ public static void main(String[] args) {
     ps.add(new PaintableOval(100,500,30,50));
     ps.add(new PaintableRectangle(100,50,130,150));
 
-    ShowInFrame.show(title:"hello", new PaintablesPanels(ps));
+    ShowInFrame.show(title:"hello", new PaintablesPanels(pa));
 }
 }
